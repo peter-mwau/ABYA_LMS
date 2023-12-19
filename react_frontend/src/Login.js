599d1238b99fc1b1869909249a5735acf69aefef
Peter Kyale <kyalepeter2000@gmail.com>
2023-12-19 18:15:50 +0300

    updated UI

--- react_frontend/src/Login.js
+++ react_frontend/src/Login.js
@@ -35,7 +35,7 @@ function Login() {
           localStorage.setItem('token', result.data.key);
           setSuccessMessage('Logged In successfully!');
           // Redirect the user to the home page
-          navigate('/home');
+          navigate('/dashboard');
           // This depends on your routing setup
         }
       })
@@ -56,22 +56,18 @@ function Login() {
               </h1>
               {/* login with google and login with github buttons */}
               <div class="flex flex-row space-x-2">
-                  <a href="#" class="flex items-center justify-center px-4 py-2 space-x-2 font-medium tracking-wide text-cyan-950 transition-colors duration-200 transform bg-gray-200 rounded-md dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 focus:ring-gray-500 dark:focus:ring-gray-600 focus:ring-2 focus:outline-none">
-                      <span>
-                      <span>
-                          {/* github icon */}
-                          <img width="30" height="30" src={githubIcon} />
-                      </span>
-                      </span>
-                      <span className='text-sm'>Login with Github</span>
-                  </a>
-                  <a href="#" class="flex items-center justify-center text-sm px-4 py-2 space-x-2 font-medium tracking-wide text-cyan-950 transition-colors duration-200 transform bg-gray-200 rounded-md dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 focus:ring-gray-500 dark:focus:ring-gray-600 focus:ring-2 focus:outline-none">
-                      <span>
-                          {/* google icon */}
-                          <img width="30" height="30" src={googleIcon} />
-                      </span>
-                      <span>Login with Google</span>
-                  </a>
+              <button type="button" class="text-white bg-gray-300 hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2">
+                <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+                </svg>
+                  Sign in with Github
+              </button>
+              <button type="button" class="text-white bg-gray-300 hover:bg-cyan-950 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
+                <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+                  <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
+                </svg>
+                  Sign in with Google
+              </button>
               </div>
               {/* or div to separate the two */}
               <div class="relative mt-4">
@@ -107,7 +103,7 @@ function Login() {
                       </div>
                   </div>
                  
-                  <button type="submit" class="w-full text-white bg-gray-300 hover:bg-yellow-500 hover:text-cyan-950 focus:ring-4 focus:outline-none focus:ring-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
+                  <button type="submit" onClick={handleSubmit} class="w-full text-white bg-gray-300 hover:bg-yellow-500 hover:text-cyan-950 focus:ring-4 focus:outline-none focus:ring-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don't have an account? <a href="/register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Signup here</a>
                   </p>
