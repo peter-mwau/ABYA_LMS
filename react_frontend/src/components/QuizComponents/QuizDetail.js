0a77e99863642c00d3196e668dc6989f2974b42a
Peter Kyale <kyalepeter2000@gmail.com>
2024-06-28 08:55:37 +0300

    debugged errors

--- react_frontend/src/components/QuizComponents/QuizDetail.js
+++ react_frontend/src/components/QuizComponents/QuizDetail.js
@@ -7,6 +7,7 @@ const QuizDetail = () => {
   const [quizData, setQuizData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  
 
   useEffect(() => {
     const fetchQuizData = async () => {
@@ -35,22 +36,29 @@ const QuizDetail = () => {
   if (!quizData) return <div>No quiz data available</div>;
 
   return (
-    <div>
-      <h1>{quizData.quiz_title}</h1>
-      <p>{quizData.quiz_description}</p>
-      {quizData.questions.map((question) => (
+    <>
+    <div className=' dark:bg-gray-800 dark:text-gray-100'>
+    <div className='container p-2 h-[100vh] md:w-[60%] md:ml-[270px] lg:mx-auto lg:w-[60%]'>
+      <h1 className='font-bold p-2'>Quiz Title: {quizData.quiz_title}</h1>
+      {/* <p>{quizData.quiz_description}</p> */}
+      {quizData.questions.map((question, index) => (
         <div key={question.id}>
-          <h3>{question.question_text}</h3>
+          <h3 className='font-semibold italic ml-2 py-2'>{index + 1}. {question.question_text}</h3>
           <ul>
             {question.choices.map((choice) => (
-              <li key={choice.id}>
+              <li key={choice.id} className='ml-3'>
+                <label>
+                  <input  type="radio" className='mx-2' name={`question_${question.id}`} value={choice.id}/>
                 {choice.text} {choice.is_correct && <strong>(Correct)</strong>}
+                </label>
               </li>
             ))}
           </ul>
         </div>
       ))}
     </div>
+    </div>
+    </>
   );
 };
 
--- react_frontend/src/components/courseComponents/CourseContent.js
+++ react_frontend/src/components/courseComponents/CourseContent.js
@@ -2,12 +2,17 @@ import React, {useState, useContext} from 'react';
 import { useParams } from 'react-router-dom';
 import { useCourseDetail } from './useCourseDetail';
 import { UserContext } from '../../contexts/userContext';
+import { Link } from 'react-router-dom';
 
 const CourseContent = () => {
   const { courseId } = useParams();
   const { courseData, loading, error } = useCourseDetail(courseId);
   const [progress, setProgress] = useState({});
   const { user } = useContext(UserContext)
+  const { quizId } = useParams();
+  // const { quiz } = useContext(QuizDetail);
+
+  // console.log("Quiz detail: ", quiz);
 
   const totalProgress = Object.values(progress).reduce((acc, curr) => acc + curr, 0) / Object.keys(progress).length || 0;
 
@@ -28,6 +33,31 @@ const CourseContent = () => {
       });
     };
 
+    courseData.chapters_with_lessons_and_quizzes.forEach(chapterWithLessonsAndQuizzes => {
+      const chapterId = chapterWithLessonsAndQuizzes.chapter.id;
+      console.log("Chapter ID:", chapterId);
+    });
+
+    // courseData.chapters_with_lessons_and_quizzes.forEach(chapter => {
+    //   chapter.quizzes.forEach(quiz => {
+    //     console.log(quiz.id); // This will log each quiz ID
+    //   });
+    // });
+
+    // Function to find a quiz ID for a given chapter ID
+    function getQuizIdForChapter(chapterId) {
+      console.log("Requested chapterId:", chapterId); // Debug log
+      let quizId = null;
+      courseData.chapters_with_lessons_and_quizzes.forEach(chapter => {
+        if (chapter.chapter_id === chapterId) {
+          quizId = chapter.quizzes.length > 0 ? chapter.quizzes[0].id : null;
+        }
+      });
+      console.log("Found quizId:", quizId); // Debug log
+      console.log("Chapter ID: ", chapterId);
+      return quizId;
+    }
+
   return (
     <div className=" mx-auto p-4 text-cyan-950 dark:bg-gray-800 dark:text-gray-100 md:pl-[270px] md:m-0 lg:mx-auto lg:px-[500px]">
       <h1 className="text-3xl font-bold mb-4 px-2">{courseData.course_name}</h1>
@@ -43,25 +73,25 @@ const CourseContent = () => {
       <div className="mb-6">
         {/* <h2 className="text-2xl font-semibold mb-2">Chapters</h2> */}
         {courseData.chapters_with_lessons.map((chapter, index) => (
-          <div key={index} className="border p-4 mb-4 rounded-lg">
-            <h3 className="text-xl font-bold">{chapter.chapter.chapter_name}</h3>
-            <p className="text-gray-700 dark:text-gray-300">{chapter.chapter.chapter_description}</p>
-
-            <div className="mt-4">
-              {/* <h4 className="text-lg font-semibold">Lessons</h4> */}
-              {chapter.lessons.map((lesson, lessonIndex) => (
-                <div key={lesson.id} className="ml-4 mt-2">
-                  <h5 className="text-gray-800 font-semibold text-xl">{`Lesson ${lessonIndex + 1}: ${lesson.lesson_name}`}</h5>
-                  <p className="text-gray-700  dark:text-gray-300">{lesson.lesson_content}</p>
-                  <p className='my-2 text-yellow-500'>Video: {lesson.video}</p>
-                  {user.user_type === "Student" && (
-                  <button onClick={markAsRead} className='bg-cyan-950 text-gray-200 rounded hover:bg-yellow-500 p-2 font-semibold'>Mark as Read</button>
-                )}
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
+  <div key={index} className="border p-4 mb-4 rounded-lg">
+    <h3 className="text-xl font-bold">{chapter.chapter.chapter_name}</h3>
+    {/* Assuming `getQuizIdForChapter` is a function that returns the quiz ID for a given chapter ID */}
+    <Link to={`/quiz-detail/${getQuizIdForChapter(quizId)}`} className="text-cyan-900 hover:text-yellow-400 underline hover:cursor-pointer">Take Quiz</Link>
+    {/* <Link to={`/quiz/${quizId}`}>Go to Quiz</Link> */}
+    <div className="mt-4">
+      {chapter.lessons.map((lesson, lessonIndex) => (
+        <div key={lesson.id} className="ml-4 mt-2">
+          <h5 className="text-gray-800 font-semibold text-xl">{`Lesson ${lessonIndex + 1}: ${lesson.lesson_name}`}</h5>
+          <p className="text-gray-700  dark:text-gray-300">{lesson.lesson_content}</p>
+          <p className='my-2 text-yellow-500'>Video: {lesson.video}</p>
+          {user.user_type === "Student" && (
+          <button onClick={markAsRead} className='bg-cyan-950 text-gray-200 rounded hover:bg-yellow-500 p-2 font-semibold'>Mark as Read</button>
+          )}
+        </div>
+      ))}
+    </div>
+  </div>
+))}
       </div>
 
       <div className="mb-6">
@@ -73,7 +103,7 @@ const CourseContent = () => {
               <h4 className="text-lg font-semibold">Quizzes</h4>
               {chapter.quizzes.map((quiz) => (
                 <div key={quiz.id} className="ml-4 mt-2">
-                  <p className="text-gray-800">{quiz.title}</p>
+                  <p className="text-gray-800">{quiz.quiz_title}</p>
                 </div>
               ))}
             </div>
