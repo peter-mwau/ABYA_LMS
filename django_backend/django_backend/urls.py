3b7058c0865bd73a865c23dc0ab7079a498ba399
AdoyoClifford <adoyoclifford@gmail.com>
2024-09-24 08:00:43 +0300

    updated the project to use docker and nginx

--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('users/', include('users.urls')),
+#     path('accounts/', include('allauth.urls')),
+#     path('courses/', include('courses.urls')),
+#     path('assignments/', include('assignments.urls')),
+#
+# ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('courses/', include('courses.urls')),
-    path('assignments/', include('assignments.urls')),
-    
+    path('api/', include([
+        path('users/', include('users.urls')),
+        path('courses/', include('courses.urls')),
+        path('assignments/', include('assignments.urls')),
+    ])),
+    path('accounts/', include('allauth.urls')),  # Keep this outside if it's not part of your API
 ]
